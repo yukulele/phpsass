yukulele/phpsass

commit 26d196f678cc3c5a2fdf2347a4c3c41c6b178a36

support mixin in mixin
use the parent context to avoid an endless recursion as not the outer content is used

tree/SassContentNode.php
@@ -51,7 +51,7 @@ class SassContentNode extends SassNode {
     $children = array();
     foreach ($context->getContent() as $child) {
       $child->parent = $this->parent;
-      $children = array_merge($children, $child->parse($pcontext));
+      $children = array_merge($children, $child->parse($pcontext->parent));
     }
     return $children;
   }

tests/phpSassTest.php
@@ -237,6 +237,10 @@ class PHPSass_TestCase extends PHPUnit_Framework_TestCase {
     $this->runSassTest('mixins.sass');
   }
 
+  public function testMixinInMixin() {
+    $this->runSassTest('mixin_in_mixin.scss');
+  }
+
   public function testMultiline() {
     $this->runSassTest('multiline.sass');
   }

tests/mixin_in_mixin.css
@@ -1,0 +1,3 @@
+body {
+    width: 50px;
+}

tests/mixin_in_mixin.scss
@@ -1,0 +1,15 @@
+@mixin mixin1() {
+    @content;
+}
+
+@mixin mixin2() {
+    @include mixin1() {
+        @content;
+    }
+}
+
+body {
+    @include mixin2() {
+        width: 50px;
+    }
+}

commit 19eb6e685dc1a7116800f962087d3b42c6313e4e

fix bug introduced with 26d196f678cc3c5a2fdf2347a4c3c41c6b178a36
variables set in mixin where not correctly passed

tree/SassContentNode.php
@@ -48,10 +48,13 @@ class SassContentNode extends SassNode {
   public function parse($pcontext) {
     $return = $this;
     $context = new SassContext($pcontext);
+
     $children = array();
     foreach ($context->getContent() as $child) {
       $child->parent = $this->parent;
-      $children = array_merge($children, $child->parse($pcontext->parent));
+      $ctx = new SassContext($pcontext->parent);
+      $ctx->variables = $pcontext->variables;
+      $children = array_merge($children, $child->parse($ctx));
     }
     return $children;
   }

tests/mixin_setvar.css
@@ -1,0 +1,3 @@
+body {
+    foo: 4;
+}

tests/mixin_setvar.scss
@@ -1,0 +1,10 @@
+@mixin layout2($otherFoo) {
+  $foo    : $otherFoo;
+  @content;
+}
+$foo  : 2;
+body {
+    @include layout2(4) {
+        foo: $foo;
+    }
+}

tests/phpSassTest.php
@@ -337,4 +337,8 @@ class PHPSass_TestCase extends PHPUnit_Framework_TestCase {
   public function testWarn() {
     $this->runSassTest('warn.sass');
   }
+
+  public function testMixinSetvar() {
+    $this->runSassTest('mixin_setvar.scss');
+  }
 }